a33e21fc81efa72e25dfbce413de978ba88253b9
pesaher <pesaher@gmail.com>
2025-11-05 23:52:42 +0100

    Revert "Update cheniles.js"
    This reverts commit 1e423f795f8699705e25e54b26ff66ce6bb3b3fd.

--- js/cheniles.js
+++ js/cheniles.js
@@ -74,15 +74,6 @@ function pintar() {
                         }
                     });
 
-                    // Asegurar que el cursor sea correcto según el modo
-                    if (modoReordenar) {
-                        marco.classList.remove('clickable');
-                        marco.style.cursor = 'grab';
-                    } else {
-                        marco.classList.add('clickable');
-                        marco.style.cursor = 'pointer';
-                    }
-
                     zona.appendChild(marco);
                 }
             });
